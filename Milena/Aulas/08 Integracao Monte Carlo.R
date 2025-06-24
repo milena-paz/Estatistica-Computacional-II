2066a2939eb375d5321b3d4076479455e2e2a7d5
Milena Paz <milenap4zfreitas@gmail.com>
2025-06-24 17:51:29 -0300

    aula 24/06

--- Milena/Aulas/08 Integracao Monte Carlo.R
+++ Milena/Aulas/08 Integracao Monte Carlo.R
@@ -2,7 +2,7 @@
 #obj: resolução de integrais definidas usando números aleatórios.
 
 
-# * Integrais do tipo theta= integral (calculada em [0,1]) de g(x)dx
+# * Integrais do tipo theta= integral (calculada em [0,1]) de g(x)dx ----
 #   Seja U~Unif(0,1):
 #   theta= E(g(U)) = integral(de 0 a 1) de g(x)*f_U(x)*dx; onde f_U(x) = 1
 #   Assim, temos o seguinte algoritmo para estimar theta:
@@ -57,3 +57,52 @@ amplitude <- apply(aprox,2, function(x) diff(range(x)))
 
 #tabela compilando tudo
 cbind(n,medias,ep,LInf=Limites[1,],LSup=Limites[2,],amplitude)
+
+## 24/06
+## * INTEGRAÇAO NO INTERVALO (a, b) ----
+# - Realizamos a transformação 
+# X = Y(b-a) + a
+# Y = (X - a) / (b-a)
+# dY = 1/(b-a) dX
+# - Substitui-se na integral de modo que os limites de integração se tornem (0,1)
+# - Volta à linha 10 da nota de aula
+
+### EXEMPLO
+# Seja X~N(0,1)
+# Deseja-se calcular por integração MC: theta = P( 1 < X < 2 )
+
+#g(x)= dnorm(x,mean=0,sd=1)
+
+U <- runif(1E3)
+t <- mean( dnorm(U+1) )
+# VALOR REAL:
+theta <- pnorm(2) - pnorm(1)
+rbind(estimativa=t,valor.real=theta)
+
+estima<- function(n){
+  U <- runif(n)
+  return(mean(dnorm(U+1)))
+}
+aprox <- t(replicate(100, sapply(n,estima)))
+
+plot(log10(n), y=1:8, ylim=c(0.10,0.15),type="n")
+for(i in 1:8){
+  points(rep(log10(n)[i],100),aprox[,i])
+}
+abline(h=theta,col="blue",lwd=2,lty=2)
+text(5,0.14,labels= bquote(theta == .(theta)),col="blue")
+
+# violin plot das estimativas
+library(vioplot)
+colnames(aprox) <- n
+vioplot(aprox, col="lightgreen")
+
+# medias das estimativas
+medias <- apply(aprox, 2, mean)
+names(medias) <- n
+medias
+
+#erro padrao das estimativas
+ep <- apply(aprox,2,sd)
+names(ep) <- n
+ep
